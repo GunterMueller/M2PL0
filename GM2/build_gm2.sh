97e67787c07e95ea7500846cce134565251b877d
GunterMueller <gunter.mueller@gmail.com>
2018-08-17 00:01:00 -0400

    Corrected GM2 build file.

--- GM2/build_gm2.sh
+++ GM2/build_gm2.sh
@@ -3,11 +3,29 @@
 # This build file assumes the vishap oberon compiler is installed.
 ############################################
 
-
 #
-for x in *.mod
+#rm -f ./*.o 
+# rm -f ./*_m2.cpp ./*_m2.s
+
+for x in   \
+  CharacterInput.mod \
+  Interpreter.mod    \
+  ErrorHandling.mod  \
+  Scanner.mod        \
+  StringTable.mod    \
+  Generator.mod     \
+  ObjectTable.mod    \
+  InternalTree.mod   \
+  SyntaxAnalysis.mod \
+  ErrorHandling.mod  \
+  Synthesis.mod      
 do
+  echo "+++++++++++++++++++++++++++++"
   echo $x
-  gm2 -g -I. -flibs=ulm $x
+  gm2  -g -I. -flibs=log,pim $x
 done
+
+  echo "+++++++++++++++++++++++++++++"
+  echo PL0.mod
+gm2  -g -I.  -flibs=log,pim PL0.mod -o PL0
 
